34cf11f0c23e721602c439da073092763b864c92
annannfryingpan <anngledson@gmail.com>
2018-06-02 17:22:19 +0100

    ReFactorised SelectedStreams classes

--- requestInfoFetchList.py
+++ requestInfoFetchList.py
@@ -21,31 +21,11 @@ class RequestInfoFetchList(RequestInfoList):
         self.requests = []
 
     def append_request(self, hub_short_title, request_params, api_key=None, username=None):
-        """try:
-            hub_api = request_params['stream_params'][0]
-        except:
-            hub_api = request_params.split(',')[0]"""
-
         self.append(RequestInfoFetchFactory.create_request_info_fetch(hub_short_title, request_params, username, api_key))
 
-        """if (hub_api == HUB_API_BT):
-            self.append(Request_info_restful_bt_fetch(username, api_key, request_params))
-        elif (hub_api == HUB_API_TRIANGULUM):
-            self.append(Request_info_osisoft_pi_fetch(request_params))
-        elif (hub_api == HUB_API_CDP):
-            self.append(Request_info_restful_cdp_fetch(api_key, request_params))
-        else:
-            raise ValueError("Unknown hub api as first value in 'stream_params' list: " + hub_api)"""
 
-
-    def append_request_list(self, request_params_list, api_key=None, username=None):
-        # import hypercat streams
-        for line in request_params_list:
-            self.append_request(line, api_key, username)
-
-
-    def append(self, stream):
-        self.requests.append(stream)
+    def append(self, request_info):
+        self.requests.append(request_info)
 
     def get_list_of_users_stream_ids(self):
         result = []
